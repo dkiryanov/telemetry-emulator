e3140389c2608de8a49abfe1d89025c4100655de
dkiryanov <d.kiryanov@tut.by>
2016-12-01 17:15:19 +0300

    Telemetry sender implementeed.

---
+++ IoTTelemetry/EventHubMessageSender/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading;
+
+
+namespace EventHubMessageSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("TELEMETRY SENDER EMULATOR");
+            Console.WriteLine("__________________________");
+            Console.ResetColor();
+
+            SendTelemetryMessages();
+
+            Console.ReadKey();
+        }
+
+        static async void SendTelemetryMessages()
+        {
+            EventHubClient eventHubClient = EventHubClient.CreateFromConnectionString(Settings.Default.EventHubConnectionString, Settings.Default.EventHubName);
+
+            Random rand = new Random();
+
+            Thread.Sleep(5000);
+
+            while (true)
+            {
+                TelemetryPoint telemetryPoint = new TelemetryPoint(rand);
+                string messageString = JsonConvert.SerializeObject(telemetryPoint);
+
+                await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(messageString)));
+
+                Console.WriteLine($"Sending message: {messageString}");
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ IoTTelemetry/EventHubMessageSender/TelemetryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EventHubMessageSender
+{
+    public class TelemetryPoint
+    {
+        public double Temparature { get; set; }
+        public double Humidity { get; set; }
+        public DateTime Time { get; set; }
+
+        public TelemetryPoint() { }
+
+        public TelemetryPoint(Random rand)
+        {
+            Temparature = Math.Round(rand.NextDouble() * 60 - 2, 2);
+            Humidity = Math.Round(rand.NextDouble() * 100 - 5, 2);
+            Time = DateTime.UtcNow;
+        }
+    }
+}
